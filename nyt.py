e54400a4d964fc926c3c4fb7155ee71495d7473b
Jonathan Cai <caimjonathan@gmail.com>
2019-05-16 18:36:52 -0700

    modularize nyt data extraction

---
+++ nyt.py
@@ -0,0 +1,97 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+import pickle
+import re
+from datetime import datetime
+from pandas import to_datetime
+
+# Functions to get time-series data for news articles using NYT's API for a given query.
+
+def get_nyt_article(url):
+    """
+    Uses BeautifulSoup to get an NYT article given its url.
+    Returns title string, url string, words is list of strings.
+    """
+    r = requests.get(url, allow_redirects=True)
+    if r.status_code >= 500:
+        print("Server error! Status code:", r.status_code, "Returning...")
+        return None, None, None, None
+    if r.status_code >= 400 and r.status_code < 500:
+        print("Bad request! Status code:", r.status_code, "Returning...")
+        return None, None, None, None
+    soup = BeautifulSoup(r.content, "html.parser")
+    title = soup.title.get_text()
+    paragraphs = "" 
+    para = soup.findAll("p", {"class":"css-1ygdjhk evys1bk0"})
+    for p in para:
+        paragraphs += p.get_text()
+
+    words = re.sub("[^\w]", " ", paragraphs).split()
+    return (title, url, words)
+
+
+def get_nyt_data(max_pages, api_key, query, pickle_bool, pickle_file_name):
+    """ 
+    Takes max number of pages (each page is 10 articles) to query, the api_key for NYT API,
+    and the query term.
+    Returns list of tuples ()
+    Optional: (boolean) pickle_bool, (string) pickle_file_name.
+    If pickle, pickles list of documents to file named pickle_file_name.
+    """
+    print("Requesting NYT API for max {} pages for query, \"{}\".".format(max_pages, query))
+    # Store all doc/publish dates as list of tuples.
+    docs = []
+    # First query to get number of hits.
+    resp = requests.get(
+        'https://api.nytimes.com/svc/search/v2/articlesearch.json',
+        params={
+            'q': query,
+            'api-key' : api_key},
+    )
+    rj = resp.json()
+    # Response.meta.hits gives us the number of pages of results.
+    hits = rj['response']['meta']['hits']
+    print("NYT found {} pages.".format(hits))
+    docs.extend([(x['web_url'], x['pub_date']) for x in rj["response"]["docs"]])
+    c = 1
+    req_start = time.time()
+    # Loop for max_pages, unless there are less hits than max_pages.
+    while c <= min(hits, max_pages):
+        resp = requests.get(
+        'https://api.nytimes.com/svc/search/v2/articlesearch.json',
+        params={
+            'q': query,
+            'api-key' : api_key,
+            'page' : c
+            },
+        )
+        docs.extend([(x['web_url'], x['pub_date']) for x in rj["response"]["docs"]])
+        print("Requesting page #{} of {}.".format(c, min(hits, max_pages)))
+        c += 1
+        # Sleep between requests to avoid hitting limit.
+        time.sleep(6)
+
+    req_finish = time.time()
+    print("Finished getting document urls/dates in {}".format(req_finish - req_start))
+
+    print("Number of docs returned from NYT:", len(docs))
+    b = len(docs)
+    print("Removing {} duplicates:".format(b - len(set(docs))))
+    docs = list(set(docs))
+    print("Extracting text from each url...")
+    # List of (url, datetime, list of words)
+    docs = [(d[0], to_datetime(d[1]), get_nyt_article(d[0])[2]) for d in docs]
+    print("Finished extracting text from urls in {}".format(time.time() - req_finish))
+
+    # If user requests pickle
+    if (pickle_bool):
+        with open(pickle_file_name, 'wb') as fp:
+            pickle.dump(docs, fp)
+
+    return docs
+
+
+api_key = "AoA9eRNg2H99U2r61TbmsEoiWxVABIjD"
+query = "Google"
+print(get_nyt_data(0, api_key, query, True, "goodstuff.txt"))
--- validation.py
+++ validation.py
@@ -1,6 +1,13 @@
-from lexicon import pos_words, neg_words
 import pickle
+import requests
+import time
+import re
 
+import multiprocessing as mp
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 with open ('positive_words.txt', 'rb') as fp:
     pos_words = pickle.load(fp)
@@ -10,3 +17,105 @@ with open ('negative_words.txt', 'rb') as fp:
 
 print(pos_words[:20])
 print(neg_words[:20])
+
+# # Validation
+# I am simply checking whether how many good/bad words are contained in the test files.
+# Using a simple rule-based algorithm - count of positive words vs negative words (the resulting sentiment for now will simply be positive - negative word counts).
+
+# Test files
+fb = "fb-bad.txt"
+fb2 = "fb-sure-bad.txt"
+goog = "goog-bad.txt"
+pix = "pixel-bad.txt"
+uber = "uber-bad.txt"
+test = "test/"
+file = test + uber
+
+def get_rule_sentiment(text):
+    """ Given a string, return number of (pos, neg) words. """
+    # Use regex to split text into list of words
+    words = re.sub("[^\w]", " ",  text).split()
+    # Get number of pos/neg words
+    pos = len([c for c in words if c.lower() in pos_words])
+    neg = len([c for c in words if c.lower() in neg_words])
+#     print("Number of positive words:", pos)    
+#     print("Number of negative words:", neg)
+    return (pos - neg)
+    
+# with open(file) as f:
+#     data = f.read()
+#     print(words[:100])
+#     display(get_rule_sentiment(data))
+
+
+
+# Let's now construct a dataframe with sentiment for all documents.
+# pool = mp.Pool(mp.cpu_count())
+# data = [[d[0], d[1], pool.apply(get_rule_sentiment, args=())]
+data = []
+i = 0
+count = 0
+for d in docs:
+    print("Processing doc #", i, "-", d[0])
+    i += 1
+    sent = get_rule_sentiment(get_nyt_article(d[0])[2])
+    if (sent == 0):
+        count += 1
+#         print("WE GOT A ZERO HERE : ", count, i)
+    data.append([d[0], d[1], sent])
+print("Number of zeros: ", count)
+
+
+df = pd.DataFrame(data, columns = ['url', 'date', 'sentiment'])
+
+# Convert date from string to datetime
+df['date'] = pd.to_datetime(df['date'])
+# Set index as date and drop the original column
+df.index = df['date']
+df.drop(columns=['date'], inplace=True)
+df.head()
+
+
+# Remove NaNs
+df.fillna(0, inplace=True)
+print("Number of zeros for sentiment:", (df['sentiment'] == 0).sum(0))
+df.astype(bool).sum(axis=0)
+
+
+
+ax = df.plot(figsize=(12,5))
+
+
+# Turns out there are duplicates resulted from NYT's API article search.
+
+# Delete duplicates
+df.drop_duplicates(subset ="url", keep=False, inplace=True) 
+pd.set_option('display.max_colwidth', -1)
+df[df['sentiment'] > 91]['url']
+
+
+
+# Now, let's get the stock data.
+API_URL = "https://www.alphavantage.co/query"
+data = {
+    "function": "TIME_SERIES_DAILY",
+    "datatype": "json",
+    "apikey": "94Z49Z19XNL1GGGP",
+    }
+data['symbol'] = "GOOG"
+response = requests.get(API_URL, data)
+
+raw = response.json()["Time Series (Daily)"]
+st = pd.DataFrame.from_dict(raw, orient = "index").reset_index(0).rename(columns = {"index": "ds"})
+st.head()
+
+
+st['ds'] = pd.to_datetime(st.ds)
+labels = ['1. open', '2. high', '3. low', '4. close', '5. volume']
+st[labels] = st[labels].apply(pd.to_numeric)
+st.index = st['ds']
+st.drop(columns=['ds'], inplace=True)
+
+
+st[datetime(2019, 3, 1):].plot(kind = "line", y = ['1. open'], figsize=(12,5))
+df.plot(figsize=(12,5))
